3594754971eff395062bea9673dce0d39e8c477a
newgamer445 <newgamer445@gmail.com>
2024-03-21 13:16:10 +0000

    second commit

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -11,7 +11,7 @@ class Loginpage extends StatefulWidget {
 class _LoginpageState extends State<Loginpage> {
   @override
   Widget build(BuildContext context) {
-    // bool checkedValue = false; // Initialize the checkedValue for the checkbox
+    bool checkedValue = false; // Initialize the checkedValue for the checkbox
 
     return Scaffold(
       appBar: AppBar(
@@ -25,102 +25,130 @@ class _LoginpageState extends State<Loginpage> {
         shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 100),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          // crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelStyle: const TextStyle(
-                      fontSize: 16, fontWeight: FontWeight.bold),
-                  hintStyle: const TextStyle(
-                      fontSize: 16, fontWeight: FontWeight.bold),
-                  labelText: "Username",
-                  hintText: 'Enter an email',
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      width: 3,
-                      color: customgreen,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 100),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelStyle: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                    hintStyle: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                    labelText: "Username",
+                    hintText: 'Enter an email',
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        width: 3,
+                        color: customgreen,
+                      ),
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      width: 3,
-                      color: customgreen,
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        width: 3,
+                        color: customgreen,
+                      ),
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 15,right: 15, top: 25),
-              child: TextField(
-                obscureText: true,
-                keyboardType: TextInputType.visiblePassword,
-                decoration: InputDecoration(
-                  labelStyle: const TextStyle(
-                      fontSize: 17, fontWeight: FontWeight.bold),
-                  hintStyle: const TextStyle(
-                      fontSize: 17, fontWeight: FontWeight.bold),
-                  labelText: "Password",
-                  hintText: 'Enter a password',
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      width: 3,
-                      color: customgreen,
-                    ),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      width: 3,
-                      color: customgreen,
-                    ),
-                    borderRadius: BorderRadius.circular(10.0),
                   ),
                 ),
               ),
-            ),
-            // Remember Me checkbox
-            // CheckboxListTile(
-            //   title: const Text(
-            //     "Remember Me",
-            //   ),
-            //   value: checkedValue,
-            //   onChanged: (newValue) {
-            //     FocusManager.instance.primaryFocus?.unfocus();
-            //     setState(() {
-            //       checkedValue = newValue!;
-            //     });
-            //   },
-            //   contentPadding: const EdgeInsets.only(left: 15, right: 15, top: 0),
-            //   controlAffinity: ListTileControlAffinity.leading,
-            // ),
-            // Forget Password button
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Padding(padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: TextButton(
-                    child: const Text(
-                      "Forget Username/Password",
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15, top: 25),
+                child: TextField(
+                  obscureText: true,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecoration(
+                    labelStyle: const TextStyle(
+                        fontSize: 17, fontWeight: FontWeight.bold),
+                    hintStyle: const TextStyle(
+                        fontSize: 17, fontWeight: FontWeight.bold),
+                    labelText: "Password",
+                    hintText: 'Enter a password',
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        width: 3,
+                        color: customgreen,
+                      ),
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
-                    onPressed: () {
-                      // Handle the onPressed event
-                    },
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        width: 3,
+                        color: customgreen,
+                      ),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
                   ),
-                )
-              ],
-            ),
-            const SizedBox(height: 50,git initwidth: 300, child: Card(color: customgreen,),)
-          ],
+                ),
+              ),
+              // Remember Me checkbox
+              // CheckboxListTile(
+              //   title: const Text(
+              //     "Remember Me",
+              //   ),
+              //   value: checkedValue,
+              //   onChanged: (newValue) {
+              //     FocusManager.instance.primaryFocus?.unfocus();
+              //     setState(() {
+              //       checkedValue = newValue!;
+              //     });
+              //   },
+              //   contentPadding: const EdgeInsets.only(left: 15, right: 15, top: 0),
+              //   controlAffinity: ListTileControlAffinity.leading,
+              // ),
+              // Forget Password button
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        SizedBox(
+                          width: 20,
+                          height: 20,
+                          child: Checkbox(
+                            checkColor: customgreen,
+                            value: checkedValue,
+                            onChanged: (newValue) {
+                              FocusManager.instance.primaryFocus?.unfocus();
+                              setState(() {
+                                checkedValue = newValue!;
+                              });
+                            },
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 15,
+                        ),
+                        const Text('Remember me')
+                      ],
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        // Handle "Forgot Password" button press
+                      },
+                      child: const Text(
+                        'Forgot Password',
+                        style: TextStyle(
+                          color: customgreen,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
